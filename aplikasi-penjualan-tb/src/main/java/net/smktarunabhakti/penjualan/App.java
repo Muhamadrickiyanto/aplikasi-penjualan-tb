c5ef570ee5194d40f15ed30cf95fed13784bc64b
Muhamadrickiyanto <lastofpower.77@gmail.com>
2014-05-16 15:21:31 +0700

    yang paling baru woy

---
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -0,0 +1,18 @@
+package net.smktarunabhakti.penjualan;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static ApplicationContext appCtx;
+    
+    public static void main( String[] args )
+    {
+        appCtx = new ClassPathXmlApplicationContext("classpath:net/smktarunabhakti/penjualan/"+"applicationContext.xml");
+    }
+}
